e9bf6185e739ba95c0d1b9ad057c6311efacc096
Dias <a14859@oficina.pt>
2026-05-05 09:27:27 +0000

    Cruzeiro java

--- gestao-equipas/src/App.java
+++ gestao-equipas/src/App.java
@@ -1,12 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Barco v1 = new Barco();
+        //Barco b1 = new Barco();
+        Cruzeiro c1 = new Cruzeiro();
 
         /*v1.setTipo("Carro");
         v1.setnRodas(4);
         v1.setVelocidadeMax(200);*/
 
-        System.out.println("Informações da Viatura:");
-        v1.mostraInfo();
+        //System.out.println("Informações da Viatura:");
+        //b1.mostraInfo();
+
+        //System.out.println("\nInformações do Cruzeiro:");
+        c1.mostraInfoCruzeiro();
+        System.out.println("\n");
     }
 }
---
+++ gestao-equipas/src/Cruzeiro.java
@@ -0,0 +1,41 @@
+public class Cruzeiro extends Barco {
+    private int numQuartos;
+    private int numPisos;
+    private String nomeBarco;
+
+
+    public Cruzeiro() {
+        numQuartos = 0;
+        numPisos = 0;
+        nomeBarco = "";
+    }
+    
+    public int getNumQuartos() {
+        return numQuartos;
+    }
+      public void setNumQuartos(int numQuartos) {
+        this.numQuartos = numQuartos;
+    }
+
+    public int getNumPisos() {
+        return numPisos;
+    }
+    public void setNumPisos(int numPisos) {
+        this.numPisos = numPisos;
+    }
+
+    public String getNomeBarco() {
+        return nomeBarco;
+    }
+    public void setNomeBarco(String nomeBarco) {
+        this.nomeBarco = nomeBarco;
+    }
+
+    public void mostraInfoCruzeiro() {
+        System.out.println("\nInformações do Cruzeiro:\n");
+        mostraInfo();
+        System.out.println("Número de Quartos: " + this.numQuartos);
+        System.out.println("Número de Pisos: " + this.numPisos);
+        System.out.println("Nome do Barco: " + this.nomeBarco);
+    }
+}
